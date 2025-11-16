08e1eb5c084c34d00170c68f8b77cce996c6c544
Guilherme Menezes <78366324+Neoxepa@users.noreply.github.com>
2025-11-16 02:51:48 -0300

    Alterações Variáveis e Classes

--- src/app/services/movie.ts
+++ src/app/services/movie.ts
@@ -2,57 +2,57 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { tap, catchError, map } from 'rxjs/operators';
-import { Movie, MovieResponse } from '../models/movie';
+import { Anime, AnimeResponse } from '../models/movie';
 
 // REQUISITO 5: Service para alocar HttpClient
 @Injectable({
   providedIn: 'root'
 })
-export class MovieService {
+export class AnimeService {
   private baseUrl = 'https://api.jikan.moe/v4';
 
   // REQUISITO 2: Uso do HttpClient
   constructor(private http: HttpClient) {}
 
-  // REQUISITO 3: Método GET - Busca filmes populares
-  getPopularMovies(page: number = 1): Observable<MovieResponse> {
-    console.log('[MovieService] getPopularMovies called', { page });
+  // REQUISITO 3: Método GET - Busca animes populares
+  getPopularAnimes(page: number = 1): Observable<AnimeResponse> {
+    console.log('[AnimeService] getPopularAnimes called', { page });
     return this.http.get<any>(
       `${this.baseUrl}/top/anime?page=${page}&sfw`
     ).pipe(
-      map((apiRes) => ({ results: apiRes.data || [], pagination: apiRes.pagination } as MovieResponse)),
-      tap((res) => console.log('[MovieService] getPopularMovies response(mapped)', { count: res.results?.length, page })),
+      map((apiRes) => ({ results: apiRes.data || [], pagination: apiRes.pagination } as AnimeResponse)),
+      tap((res) => console.log('[AnimeService] getPopularAnimes response(mapped)', { count: res.results?.length, page })),
       catchError((err) => {
-        console.error('[MovieService] getPopularMovies error', err);
+        console.error('[AnimeService] getPopularAnimes error', err);
         return throwError(() => err);
       })
     );
   }
 
-  getMovieDetails(id: number): Observable<Movie> {
-    console.log('[MovieService] getMovieDetails called', { id });
+  getAnimeDetails(id: number): Observable<Anime> {
+    console.log('[AnimeService] getAnimeDetails called', { id });
     return this.http.get<any>(
       `${this.baseUrl}/anime/${id}`
     ).pipe(
-      map((apiRes) => apiRes.data as Movie),
-      tap((res) => console.log('[MovieService] getMovieDetails response(mapped)', { id, hasData: !!res })),
+      map((apiRes) => apiRes.data as Anime),
+      tap((res) => console.log('[AnimeService] getAnimeDetails response(mapped)', { id, hasData: !!res })),
       catchError((err) => {
-        console.error('[MovieService] getMovieDetails error', { id, err });
+        console.error('[AnimeService] getAnimeDetails error', { id, err });
         return throwError(() => err);
       })
     );
   }
 
-  // REQUISITO 3: Método GET - Busca filmes por termo
-  searchMovies(query: string, page: number = 1): Observable<MovieResponse> {
-    console.log('[MovieService] searchMovies called', { query, page });
+  // REQUISITO 3: Método GET - Busca animes por termo
+  searchAnimes(query: string, page: number = 1): Observable<AnimeResponse> {
+    console.log('[AnimeService] searchAnimes called', { query, page });
     return this.http.get<any>(
       `${this.baseUrl}/anime?q=${encodeURIComponent(query)}&page=${page}`
     ).pipe(
-      map((apiRes) => ({ results: apiRes.data || [], pagination: apiRes.pagination } as MovieResponse)),
-      tap((res) => console.log('[MovieService] searchMovies response(mapped)', { query, count: res.results?.length, page })),
+      map((apiRes) => ({ results: apiRes.data || [], pagination: apiRes.pagination } as AnimeResponse)),
+      tap((res) => console.log('[AnimeService] searchAnimes response(mapped)', { query, count: res.results?.length, page })),
       catchError((err) => {
-        console.error('[MovieService] searchMovies error', { query, err });
+        console.error('[AnimeService] searchAnimes error', { query, err });
         return throwError(() => err);
       })
     );
