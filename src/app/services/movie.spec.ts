e3a6f8ac105cadcc7eb79d63299c160d1ee7bd75
Guilherme Menezes <78366324+Neoxepa@users.noreply.github.com>
2025-11-16 02:55:37 -0300

    Alterações Variáveis e Classes

--- src/app/services/movie.spec.ts
+++ src/app/services/movie.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { Movie } from './movie';
+import { AnimeService } from './movie';
 
-describe('Movie', () => {
-  let service: Movie;
+describe('AnimeService', () => {
+  let service: AnimeService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(Movie);
+    service = TestBed.inject(AnimeService);
   });
 
   it('should be created', () => {
